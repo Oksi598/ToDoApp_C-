865fab6245041b2e1cf33bc934eda9521a2c2249
Oksi598 <radovunoksana@gmail.com>
2024-11-08 18:33:08 +0200

    to fix

--- ToDoApp.DAL/Models/Task.cs
+++ ToDoApp.DAL/Models/Task.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace ToDoApp.DAL.Models
 {
     public class Task
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
